c47e118a485384ae76e260f339cc58a88431be0f
大胃王 <253498229@qq.com>
2018-06-13 18:00:29 +0800

    initial commit

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,73 @@
 package com.example;
 
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author wangbin
+ */
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+
+  @RestController
+  class TestController {
+    @Resource
+    private RuntimeService runtimeService;
+
+    @Resource
+    private TaskService taskService;
+
+    @GetMapping("/list")
+    public List<Map> list() {
+      List<Task> tasks = taskService.createTaskQuery().list();
+      return tasks.stream().map(t -> {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", t.getId());
+        map.put("name", t.getName());
+        return map;
+      }).collect(Collectors.toList());
+    }
+
+    @GetMapping("/complete/{taskId}")
+    public boolean complete(@PathVariable("taskId") String taskId) {
+      taskService.complete(taskId);
+      return true;
+    }
+
+    @GetMapping("/create")
+    public boolean create() {
+      runtimeService.startProcessInstanceByKey("LeaveProcess");
+      return true;
+    }
+
+  }
+  /*@Bean
+  CommandLineRunner runner(
+          final RepositoryService repositoryService,
+          final RuntimeService runtimeService,
+          final TaskService taskService
+  ) {
+    return args -> {
+      System.out.println("Number of process definitions : "
+              + repositoryService.createProcessDefinitionQuery().count());
+      System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+      runtimeService.startProcessInstanceByKey("choosecourse");
+      System.out.println("Number of tasks after process start: " + taskService.createTaskQuery().count());
+    };
+  }*/
 }
--- src/test/java/com/example/ApplicationTest.java
+++ src/test/java/com/example/ApplicationTest.java
@@ -1,15 +1,60 @@
 package com.example;
 
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.Deployment;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+import static org.junit.Assert.assertNull;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
+  @Resource
+  private RuntimeService runtimeService;
+  @Resource
+  private TaskService taskService;
+
+  /**
+   * 最简单的一套流程处理
+   */
   @Test
-  public void contextLoads() {
+  @Deployment
+  public void simpleProcessTest() {
+    // 开启请假流程
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("LeaveProcess");
+    // 获取当前任务节点
+    Task task = getTask(processInstance);
+    System.out.println(task.getName());
+
+    // 员工完成申请操作
+    taskService.complete(task.getId());
+    task = getTask(processInstance);
+    System.out.println(task.getName());
+
+    // 经理完成审批操作
+    taskService.complete(task.getId());
+    task = getTask(processInstance);
+
+    // 判断任务是否结束
+    assertNull(task);
+  }
+
+  /**
+   * 根据流程示例查询当前任务节点
+   *
+   * @param processInstance 流程实例
+   * @return 当前任务节点
+   */
+  private Task getTask(ProcessInstance processInstance) {
+    return taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
   }
 
 }
